6ef4c7b88f4a7fa00dd8149a92da15f6584ce74d
BolshanDV <92440782+BolshanDV@users.noreply.github.com>
2022-04-18 17:26:38 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "fstream"
+#include "codeAnalyzer/Analyzer.h"
+
+using namespace std;
+
+string readingFile() {
+    ifstream file("/Users/dmitrij/CLionProjects/codeAnalyzer/text/source.txt");
+    string str;
+    string content;
+    while (getline(file, str)) {
+        content += str;
+        content.push_back('\n');
+    };
+    return content;
+}
+
+int main() {
+    Analyzer analyzer;
+    analyzer.codeHandler(readingFile());
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
